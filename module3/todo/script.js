f0c707d24204060aac092d5970b3e1f09ee33b21
rishavkr07 <rishav0797@gmail.com>
2022-05-27 15:41:08 +0530

    functionality added

---
+++ module3/todo/script.js
@@ -0,0 +1,34 @@
+let inputTag = document.querySelector("input");
+let ulTag = document.querySelector("ul");
+
+inputTag.addEventListener('keydown',function(e){
+    console.log(e);
+    let key = e.key;
+    console.log(key);
+
+     if(key=='Enter'){
+         let value=inputTag.value;
+         console.log(value);
+
+         inputTag.value=" ";
+
+    let liTag=document.createElement("li");
+    
+    // <i class="fa fa-trash"></i>
+    //liTag.innerText=value;                            //innerText  ------>  write text
+
+    liTag.innerHTML= `   <div> ${value} </div>         
+                         <div class="delete"> <i class="fa fa-trash"></i></div>`
+
+  
+    handleRemoval(liTag);
+    ulTag.appendChild(liTag);
+     }
+})
+
+function handleRemoval(liTag){
+    let deletebtn = liTag.querySelector(".delete");
+    deletebtn.addEventListener('click',function(){
+        liTag.remove();
+    })
+}
